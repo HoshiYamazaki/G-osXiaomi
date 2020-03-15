ed00a6f31d80336142411476fcc7cfb24d21f583
HoshiYamazaki <0xGumiZoid>
2020-03-15 12:23:05 +0100

    GlosXiaomi finally working and reborked.

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from flask import Flask, request
+from argparse import ArgumentParser
+from aiohttp import web, ClientSession
 
-app = Flask(__name__)
+from json_utils import read_json_file
+
+config = read_json_file('config.json')
+parser = ArgumentParser()
+parser.add_argument('--port')
+parser.add_argument('--password', required=True)
+args = parser.parse_args()
 
 
-@app.route('/')
-def hello():
-    password = request.headers.get('pass')
-    if password == 'xiaomilepsze':
-        return 'done'
-    return 'failure'
+async def hello(request):
+    """Return response and send message to discord."""
+    if request.headers.get('pass', None) == args.password:
+        async with ClientSession() as session:
+            response = await session.post(config['webhook_uri'], json={
+                'content': 'Xiaomi daje znak!',
+            })
+            if response and response.status in [200, 204]:
+                return web.Response(text='GlosXiaomi SUCCESS.')
+    return web.Response(text='GlosXiaomi FAILURE.')
 
 
-app.run()
+async def post_info(request):
+    """Return stupid message if somebody will call this port with post."""
+    return web.Response(text='No voice of xiaomi here...')
+
+
+app = web.Application()
+app.add_routes([
+    web.post('/', hello),
+    web.get('/', post_info),
+])
+web.run_app(app, port=args.port)
